2f300e65c666bc01d570feda5263b1c31f77ba8c
Mayuri Bobade <your-email@gmail.com>
2026-06-20 00:50:21 +0530

    fixed assets path error

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,61 +1,61 @@
 const photos = [
   {
     id: 1,
-    img: "/images/1.jpeg",
+    img: "./images/1.jpeg",
     text: "First smile moment ❤️",
     caption: "This is a first selfie you send me ❤️"
   },
   {
     id: 2,
-    img: "/images/2.jpeg",
+    img: "./images/2.jpeg",
     text: "Cute unexpected memory 🥺",
     caption: "Honestly, ha photo baghitla ki kahi seconds fakta tumchyakade baghat basavasa vatat. 🥹❤️"
   },
   {
     id: 3,
-    img: "/images/3.jpeg",
+    img: "./images/3.jpeg",
     text: "Happy vibes 😄",
     caption: "Pure happiness in one frame ✨,just a killer Samile 🫠😍"
   },
   {
     id: 4,
-    img: "/images/4.jpeg",
+    img: "./images/4.jpeg",
     text: "Deep talks 💬",
     caption: "Mi tumhala kiti vela baghitla asel, pan ajun pan baghitlyavar smaile yete. Still my favorite face 😌❤️  "
   },
   {
     id: 5,
-    img: "/images/5.jpeg",
+    img: "./images/5.jpeg",
     text: "Comfort zone 🤍",
     caption: "Tumhala mahit pan nasel, pan ha photo majhya favorites madhla ek aahe. 🫶🏻❤️"
   },
   {
     id: 6,
-    img: "/images/6.jpeg",
+    img: "./images/6.jpeg",
     text: "Crazy laughter 😂",
     caption: "Kadhi kadhi vatat, thank God mi tumhala bhetle. ❤️"
   },
   {
     id: 7,
-    img: "/images/7.jpeg",
+    img: "./images/7.jpeg",
     text: "Special day ✨",
     caption: "Always and forever my favorite person ❤️ 👑"
   },
   {
     id: 8,
-    img: "/images/8.jpeg",
+    img: "./images/8.jpeg",
     text: "Night talks 🌙",
     caption: "Saglya taklya photo madhala ha best takala kelela photo . 😂 just takala manus ❤️"
   },
   {
     id: 9,
-    img: "/images/9.jpeg",
+    img: "./images/9.jpeg",
     text: "Miss you moment 🥺",
     caption: "Hay ye marathi look kiti god distoy 😘 , najar hi nhi hatey <3"
   },
   {
     id: 10,
-    img: "/images/10.jpeg",
+    img: "./images/10.jpeg",
     text: "Forever bond ❤️",
     caption: "Out of all the people in this world, majha heart ne tumhalach choose kela... ani to decision ajun pan best aahe. ❤️‍🩹"
   }
